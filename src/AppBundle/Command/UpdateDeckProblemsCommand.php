5e0e39c89170b20c3c0f4ac16459e468f85282a9
Francisco Franco Eslava <fafranco82@gmail.com>
2017-10-25 08:57:46 +0200

    Update deck problems command

---
+++ src/AppBundle/Command/UpdateDeckProblemsCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+class UpdateDeckProblemsCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+        ->setName('app:decks:problems')
+        ->setDescription('Update old decks problems after a change of rules')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $validator = $this->getContainer()->get('deck_validation_helper');
+
+        $count = 0;
+        
+        $decks = $em->getRepository('AppBundle:Deck')->findAll();
+        foreach($decks as $deck)
+        {
+            $currentProblem = $deck->getProblem();
+            $newProblem = $validator->findProblem($deck);
+            if($currentProblem != $newProblem)
+            {
+                $count++;
+                $deck->setProblem($newProblem);
+            }
+        }
+        $em->flush();
+        $output->writeln(date('c') . " $count decks have been updated its problem.");
+    }
+}
